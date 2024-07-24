40ea01824d06b46ecc817a2748365c1ac79402fd
Mahmud Apak <apakmahmud@gmail.com>
2024-07-24 22:41:40 +0200

    ADD: Stop function on mouse move to right at any point

--- sell.js
+++ sell.js
@@ -3,16 +3,40 @@ const delay = require("delay");
 let counter = 0;
 
 async function sellAlbionItems() {
+  var mouse = robot.getMousePos();
+  if (mouse.x > 1130) {
+    stop();
+  }
   robot.moveMouse(1300, 430);
   await delay(88);
+  var mouse = robot.getMousePos();
+  if (mouse.x > 1300) {
+    stop();
+  }
   robot.mouseClick();
   await delay(88);
+  var mouse = robot.getMousePos();
+  if (mouse.x > 1300) {
+    stop();
+  }
   robot.moveMouse(830, 640);
   await delay(88);
+  var mouse = robot.getMousePos();
+  if (mouse.x > 830) {
+    stop();
+  }
   robot.mouseClick();
   await delay(88);
+  var mouse = robot.getMousePos();
+  if (mouse.x > 830) {
+    stop();
+  }
   robot.moveMouse(1130, 740);
   await delay(88);
+  var mouse = robot.getMousePos();
+  if (mouse.x > 1130) {
+    stop();
+  }
   robot.mouseClick();
 
   counter++;
@@ -24,6 +48,10 @@ async function sellAlbionItems() {
 async function start() {
   await delay(2000);
   sellAlbionItems();
+}
+
+async function stop() {
+  counter = 999999;
 }
 
 start();
